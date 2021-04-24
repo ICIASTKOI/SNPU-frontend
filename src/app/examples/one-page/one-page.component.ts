61f963b82a8c95d5c566e669e1f0603676826a6a
Hubert Szczęch <hubertszczech@MacBook-Pro-Hubert.local>
2021-04-24 23:25:49 +0200

    somne changes

---
+++ src/app/examples/one-page/one-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-one-page',
+  templateUrl: './one-page.component.html',
+  styleUrls: ['./one-page.component.css']
+})
+export class OnePageComponent implements OnInit {
+
+  focus: any;
+  focus1: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
